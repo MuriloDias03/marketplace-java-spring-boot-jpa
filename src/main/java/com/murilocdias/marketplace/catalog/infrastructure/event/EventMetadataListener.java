b2e48ee2d9d8cda7ba4820eac0f35bcb3c747592
Murilo <murilocristovaodias@gmail.com>
2026-06-13 12:26:28 -0300

    feature: configuração do mongodb para EventMetada.

---
+++ src/main/java/com/murilocdias/marketplace/catalog/infrastructure/event/EventMetadataListener.java
@@ -0,0 +1,27 @@
+package com.murilocdias.marketplace.catalog.infrastructure.event;
+
+import com.murilocdias.marketplace.catalog.infrastructure.persistence.entity.Event;
+import com.murilocdias.marketplace.catalog.infrastructure.persistence.entity.EventMetadata;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.mongodb.core.mapping.event.AbstractMongoEventListener;
+import org.springframework.data.mongodb.core.mapping.event.AfterDeleteEvent;
+import org.springframework.data.mongodb.core.mapping.event.AfterSaveEvent;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EventMetadataListener extends AbstractMongoEventListener<EventMetadata> {
+
+    private static final Logger logger = LoggerFactory.getLogger(EventMetadataListener.class);
+
+    @Override
+    public void onAfterSave(AfterSaveEvent<EventMetadata> event) {
+        logger.info("Event metadata save via onAfterSave {}", event.getDocument());
+    }
+
+    @Override
+    public void onAfterDelete(AfterDeleteEvent<EventMetadata> event) {
+        logger.info("Event metadata delete via onAfterDelete {}", event.getDocument());
+    }
+
+}
